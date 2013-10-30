65f9e707766b1157e305eb8a34a860f4f096b20d
Steven Yi <stevenyi@gmail.com>
2013-10-30 14:42:28 -0400

    translated examples 1 and 2 to clojure

--- clojure/example1.clj
+++ clojure/example1.clj
@@ -1,6 +1,22 @@
-;; Example 1
-;;
-;;
+; Example 1 - Simple Compilation with Csound
+; Author: Steven Yi <stevenyi@gmail.com>
+; 2013.10.30
+;
+; This example is a barebones example for creating an instance of Csound, 
+; compiling a pre-existing CSD, calling Perform to run Csound to completion,
+; then Stop and exit.  
+;
+; The first thing we do is import the csnd6 module, which is the module 
+; containing the Python interface to the Csound API.
 
-(require csnd6)
+(import (csnd6 csnd6 Csound))
 
+; this line turns off Csound's atexit handler as well as signal handlers
+(csnd6/csoundInitialize (bit-or csnd6/CSOUNDINIT_NO_ATEXIT csnd6/CSOUNDINIT_NO_SIGNAL_HANDLER))
+
+(let [c (Csound.)]      ; Create an instance of the Csound object 
+  (.Compile c "test1.csd")    ; Compile a pre-defined test1.csd file
+  (.Perform c)                ; This call runs Csound to completion
+  (.Stop c))                  ; At this point, Csound is already stopped, but this call is here
+                              ; as it is something that you would generally call in real-world 
+                              ; contexts 
---
+++ clojure/example2.clj
@@ -0,0 +1,41 @@
+; Example 2 - Compilation with Csound without CSD
+; Author: Steven Yi <stevenyi@gmail.com>
+; 2013.10.28
+;
+; In this example, we move from using an external CSD file to 
+; embedding our Csound ORC and SCO code within our Python project.
+; Besides allowing encapsulating the code within the same file,
+; using the CompileOrc() and CompileSco() API calls is useful when
+; the SCO or ORC are generated, or perhaps coming from another 
+; source, such as from a database or network.
+
+(import (csnd6 csnd6 Csound))
+
+; this line turns off Csound's atexit handler as well as signal handlers
+(csnd6/csoundInitialize (bit-or csnd6/CSOUNDINIT_NO_ATEXIT csnd6/CSOUNDINIT_NO_SIGNAL_HANDLER))
+
+; Defining our Csound ORC code within a triple-quoted, multline String
+(def orc "
+sr=44100
+ksmps=32
+nchnls=2
+0dbfs=1
+
+instr 1 
+aout vco2 0.5, 440
+outs aout, aout
+endin")
+
+
+; Defining our Csound SCO code 
+(def sco "i1 0 1")
+
+(let [c (Csound.)]
+ (.SetOption c "-odac") ; Using SetOption() to configure Csound
+                        ; Note: use only one commandline flag at a time
+ (.CompileOrc c orc)    ; Compile the Csound Orchestra String
+ (.ReadScore c sco)     ; Compile the Csound SCO String 
+ (.Start c)             ; When compiling from strings, this call is necessary before doing any performing 
+ (.Perform c)           ; Run Csound to completion
+ (.Stop c))
+
