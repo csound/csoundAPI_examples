231333b1b14e22527d4e8871baaacec1a021871b
Steven Yi <stevenyi@gmail.com>
2013-11-04 23:05:18 -0500

    initial translation of example 1 to C

---
+++ c/example1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <csound.h>
+
+/**
+ * TODO - Add comments!
+ */
+
+int main(int arg, char** argv) {
+
+  csoundInitialize(CSOUNDINIT_NO_ATEXIT);
+
+  CSOUND* csound = csoundCreate(NULL);
+  char* args[2];
+  args[0] = "csound";
+  args[1] = "test1.csd";
+
+  csoundCompile(csound, 2, args);  
+  csoundPerform(csound);
+  csoundStop(csound);
+}
