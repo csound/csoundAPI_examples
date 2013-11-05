70425bb2ff7a3f526f2e3c5610c909fe542a68d4
Richard Henninger <richard@rghmusic.com>
2013-11-04 21:20:07 -0500

    Initial C# examples (1-3,5-7) for Csound API

---
+++ csharp/Example2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+using csound6netlib;    //Exposes DotNet to Csound 6 Bridge's classes from csound6netlib.dll
+
+namespace csoundAPI_examples
+{
+    public partial class CsoundAPI_Examples
+    {
+        /* Example 2 - Compilation with Csound without CSD
+         * 
+         * In this example, we move from using an external CSD file to 
+         * embedding our Csound ORC and SCO code directly in our C# project.
+         * Besides allowing encapsulation of the code within the same file,
+         * using the CompileOrc() and CompileSco() API calls is useful when
+         * the SCO or ORC are generated, or perhaps coming from another 
+         * source, such as from a database or network.
+         */
+        public void Example2()
+        {
+            using (var c = new Csound6Net())
+            {
+                // Using SetOption() to configure Csound: here to output in realtime
+                c.SetOption("-odac");    // Note: SetOption uses only one commandline flag per call
+                
+                c.CompileOrc(orc);       // Compile the Csound Orchestra string
+                c.ReadScore("i1 0 1\n");   // Compile the Csound score as a string constant
+
+                c.Start();  // When compiling from strings, Start() is needed before performing
+                c.Perform();// Run Csound to completion
+                c.Stop();   // At this point, Csound is already stopped, but this call is here
+            }               // as it is something that you would generally call in real-world 
+        }                   // contexts.                    
+
+
+    }
+}
---
+++ csharp/Example6.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using csound6netlib;  //Exposes DotNet to Csound 6 Bridge's classes from csound6netlib.dll
+
+namespace csoundAPI_examples
+{
+    public partial class CsoundAPI_Examples
+    {
+        /* # Example 6 - Generating a Score
+          * 
+         * This example continues on from Example 5, rewriting the example using
+         * a Class called Note (in another class file for better reuse).
+         * The note example has its ToString() method implemented
+         * to generate a well-formatted Csound SCO note.  
+         *
+         * This example also shows how a list of notes could be used multiple times.
+         * The first loop through we use the notes as-is, and during the second time
+         * we generate the notes again with the same properties except we alter the 
+         * fifth p-field up 4 semitones. 
+         *
+         * Note: Altering a Notes values like this is alright for this example, but 
+         * it is a destructive edit.  Real world code might make copies of Notes or 
+         * alter the score generation to maintain the original values. 
+         */
+        public void Example6()
+        {
+            var r = new Random();
+            using (var c = new Csound6Net())
+            {
+                c.SetOption("-odac");   // Set option for Csound
+                //Create score from a generated list of Note objects using Midi pitches 60-75.
+                var notes = new List<Note>();
+                for (int i = 0; i < 13; i++)
+                    notes.Add(new Note(new double[] { 1, i * .25, .25, .5, r.Next(60, 75) }));
+
+                //Capture the note values as Csound parameters in a .net System.Text.StringBuilder
+                var sco = new StringBuilder();
+                foreach (var n in notes)
+                    sco.AppendLine(n.ToString());
+
+                //Add a major third and time displacement to the Note objects and put them in the score as well.
+                foreach (var n in notes)
+                {
+                    if (n.Pfields.Length > 4) n.Pfields[4] += 4; //pitch (p4)
+                    if (n.Pfields.Length > 1) n.Pfields[1] += .125; //start time (p1)
+                    sco.AppendLine(n.ToString());
+                }
+
+                try
+                {
+                    //Now that the score has been generated, apply it to Csound:
+                    c.CompileOrc(orc2);         //Compile the matching orchestra
+                    c.ReadScore(sco.ToString());//and apply the generated score
+
+                    c.Start();
+                    while (!c.PerformKsmps()) ;
+                    c.Stop();
+                }
+                catch (Csound6NetException e)
+                {
+                    System.Console.WriteLine(string.Format("Csound failed to complete: {0}", e.Message));
+                }
+            }
+        }
+
+    }
+}
