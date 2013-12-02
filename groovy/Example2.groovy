18bc7173a8923c295b4c1bb857d6faff44b52ea6
Steven Yi <stevenyi@gmail.com>
2013-12-02 18:41:54 -0500

    initial translation of example 1-5 to Groovy

---
+++ groovy/Example2.groovy
@@ -0,0 +1,57 @@
+/**
+ * Example 2 - Compilation with Csound without CSD 
+ * Author: Steven Yi <stevenyi@gmail.com>
+ * 2013.10.28
+ *
+ * In this example, we move from using an external CSD file to embedding our
+ * Csound ORC and SCO code within our Python project. Besides allowing
+ * encapsulating the code within the same file, using the CompileOrc() and
+ * CompileSco() API calls is useful when the SCO or ORC are generated, or
+ * perhaps coming from another source, such as from a database or network.
+ *
+ * @author stevenyi
+ */
+import csnd6.csnd6;
+import csnd6.Csound;
+
+csnd6.csoundInitialize(
+        csnd6.CSOUNDINIT_NO_ATEXIT | csnd6.CSOUNDINIT_NO_SIGNAL_HANDLER);
+
+// Defining our Csound ORC code within a String
+String orc = """
+sr = 44100
+ksmps = 32
+nchnls = 2
+0dbfs = 1
+
+instr 1
+aout vco2 0.5, 440
+outs aout, aout
+endin
+"""
+
+// Defining our Csound SCO code 
+String sco = "i1 0 1";
+
+// Create an instance of the Csound object
+Csound c = new Csound();
+
+// Using SetOption() to configure Csound
+// Note: use only one commandline flag at a time 
+c.SetOption("-odac");  
+
+// Compile the Csound Orchestra string
+c.CompileOrc(orc);     
+
+// Compile the Csound SCO String
+c.ReadScore(sco);
+
+// When compiling from strings, this call is necessary before doing 
+// any performing
+c.Start();
+
+// This call runs Csound to completion
+c.Perform();
+
+c.Stop();
+
---
+++ groovy/Example3.groovy
@@ -0,0 +1,61 @@
+/**
+ * Example 3 - Using our own performance loop 
+ * Author: Steven Yi <stevenyi@gmail.com>
+ * 2013.10.28
+ *
+ * In this example, we use a while loop to perform Csound one audio block at a
+ * time. This technique is important to know as it will allow us to do further
+ * processing safely at block boundaries. We will explore the technique further
+ * in later examples.
+ *
+ * @author stevenyi
+ */
+
+import csnd6.csnd6;
+import csnd6.Csound;
+
+csnd6.csoundInitialize(
+        csnd6.CSOUNDINIT_NO_ATEXIT | csnd6.CSOUNDINIT_NO_SIGNAL_HANDLER);
+
+// Defining our Csound ORC code within a String
+String orc = """sr = 44100
+        ksmps = 32
+        nchnls = 2
+        0dbfs = 1
+
+        instr 1
+        aout vco2 0.5, 440
+        outs aout, aout
+        endin""";
+
+// Defining our Csound SCO code 
+String sco = "i1 0 1";
+
+// Create an instance of the Csound object
+Csound c = new Csound();
+
+// Using SetOption() to configure Csound
+// Note: use only one commandline flag at a time 
+c.SetOption("-odac");
+
+// Compile the Csound Orchestra string
+c.CompileOrc(orc);
+
+// Compile the Csound SCO String
+c.ReadScore(sco);
+
+// When compiling from strings, this call is necessary before doing 
+// any performing
+c.Start();
+
+// The following is our main performance loop. We will perform one block 
+// of sound at a time and continue to do so while it returns 0, which 
+// signifies to keep processing.  We will explore this loop technique in 
+// further examples.
+
+while (c.PerformKsmps() == 0) {
+    // pass for now
+}
+
+c.Stop();
+
