21f30e748d97dce36761db63495d53daa67ad9f7
rorywalsh <rorywalsh@ear.ie>
2017-11-22 10:16:41 +0000

    removed comments at top

--- cpp/SimpleCsdTextPlayback.cpp
+++ cpp/SimpleCsdTextPlayback.cpp
@@ -1,12 +1,4 @@
-/* Example 1 - Simple Compilation with Csound
- Author: Steven Yi <stevenyi@gmail.com> 2013.10.28
- Adapted for C++ by Rory Walsh
-
- Adapted to C++ by Rory Walsh 
- This example is a barebones example for creating an instance of Csound, 
- compiling a pre-existing CSD, calling Perform to run Csound to completion,
- then Stop and exit.  
-*/
+/* Simple Compilation with csoundCompileCsdText() */
 
 #include <stdio.h>
 #include "csound.hpp"
