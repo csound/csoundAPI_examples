ecde72e8df9a6d1ae809344535b102d5957d3036
NMojica <nmojica@signalsecure.com>
2019-02-01 13:51:22 -0600

    Replaced the version in the cargo files for '*', which will indicate to cargo to use the most recent version

--- rust/example1/src/main.rs
+++ rust/example1/src/main.rs
@@ -1,37 +1,21 @@
+use std::env;
 extern crate csound;
 use csound::*;
 
-static CSD: &str = "<CsoundSynthesizer>
-<CsOptions>
--odac
-</CsOptions>
-<CsInstruments>
-sr = 44100
-ksmps = 32
-nchnls = 2
-0dbfs  = 1
-instr 1
-kamp = .6
-kcps = 440
-ifn  = p4
-asig oscil kamp, kcps, ifn
-     outs asig,asig
-endin
-</CsInstruments>
-<CsScore>
-f1 0 16384 10 1
-i 1 0 2 1
-e
-</CsScore>
-</CsoundSynthesizer>";
-
 fn main() {
-    let mut cs = Csound::new();
+    /* Receive a command line argument, which is the path to a csound file*/
+    let args: Vec<String> = env::args().collect();
 
-    let args = ["csound", CSD];
+    if args.len() < 2 {
+        panic!("not enough arguments - please pass the path to a csound file");
+    }
+
+    /* Creates a new csound instance */
+    let cs = Csound::new();
+
+    let args = ["csound", &args[1]];
+
     cs.compile(&args).unwrap();
-
-    cs.start().unwrap();
-
     cs.perform();
+    cs.stop();
 }
